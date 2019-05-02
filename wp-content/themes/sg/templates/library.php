3103af011cb579b5fd8df0a58edadc33ba0b6eaa
Xin Yin <xinyin@Xins-iMac.lan>
2019-05-02 23:24:19 +1000

    polish library page

--- wp-content/themes/sg/templates/library.php
+++ wp-content/themes/sg/templates/library.php
@@ -1,45 +1,79 @@
-<?php 
+<?php
 //
 // use this to gererate image for text file
 // https://php.net/manual/en/function.imagettftext.php
-if ( is_user_logged_in() ):
+if (is_user_logged_in()) :
 
-    global $current_user;
-    wp_get_current_user();
-    $author_query = array('posts_per_page' => '-1','author' => $current_user->ID,'post_type' => 'attachment','post_status' => 'inherit');
-    $author_posts = new WP_Query($author_query);
-    while($author_posts->have_posts()) : $author_posts->the_post();
-    ?>
+	global $current_user;
+	wp_get_current_user();
+	$date = '';
+	$author_query = array('posts_per_page' => '-1', 'author' => $current_user->ID, 'post_type' => 'attachment', 'post_status' => 'inherit', 'orderby' => 'publish_date','order' => 'DESC');
+	$author_posts = new WP_Query($author_query);
+	while ($author_posts->have_posts()) : $author_posts->the_post();
 
-        <!-- <a target="_blank" href="<?php echo wp_get_attachment_url( get_the_ID() ); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>        -->
-        <a class="sg_wordlist" target="_blank" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" data-wordlist_id="<?= get_the_ID(); ?>"><?php the_title(); ?></a>       
-    <?php           
-    endwhile;
+
+	if ( $date != get_the_date('Y/m/d') ) {
+		$date = get_the_date('Y/m/d');
+		printDate($date);
+		echo '<br />';
+		
+	}
+	the_title(); // or whatever you want here.
+	echo '<br />';
+
+		?>
+
+		<!-- <a target="_blank" href="<?php echo wp_get_attachment_url(get_the_ID()); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>        -->
+		<!-- <a class="sg_wordlist" target="_blank" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" data-wordlist_id="<?= get_the_ID(); ?>"><?php the_title(); ?></a> -->
+	<?php
+endwhile;
 
 else :
 
-    echo "not logged in";
+	echo "not logged in";
 
 endif;
+
+
+function printDate($date){
+	echo '<div class="app-calendar">';
+	echo '<div id="weekday">monday</div>';
+	echo '<div id="day">'.$date.'</div>';
+	echo '</div>';
+}
+
+
+function printBook($date){
+	echo '<div class="app-calendar">';
+	echo '<div id="weekday">monday</div>';
+	echo '<div id="day">'.$date.'</div>';
+	echo '</div>';
+}
 
 ?>
 
 
-	<div class="bookshelf">
-		<div class="shelf">
-			<div class="row-1">
-				<div class="loc">
-					<div> <div class="sample book1" sample="steve-jobs"></div> </div>
-					<div> <div class="sample book2" sample="html5"></div> </div>
-					<div> <div class="sample book3" sample="docs"></div> </div>
-				</div>
-			</div>
-			<div class="row-2">
-				<div class="loc">
-					<div> <div class="sample book4" sample="magazine1"></div> </div>
-					<div> <div class="sample book5" sample="magazine2"></div> </div>
-					<div> <div class="sample book6" sample="magazine3"></div> </div>
-				</div>
-			</div>
+
+	
+
+	
+
+
+
+	<div class="shelf">
+		<div class="shelf-row">
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
+		</div>
+		<div class="shelf-row">
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
+		</div>
+		<div class="shelf-row">
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
+			<div class="book" sample="steve-jobs"></div>
 		</div>
 	</div>
