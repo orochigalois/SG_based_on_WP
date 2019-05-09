8ada7bb4f9677b379fa270efb1964403d5f7b430
Xin Yin <xinyin@Xins-iMac.lan>
2019-05-09 17:52:09 +1000

    finish roadmap widget

--- wp-content/themes/sg/partials/widget_roadmap.php
+++ wp-content/themes/sg/partials/widget_roadmap.php
@@ -1,1 +1,20 @@
-<h1>test</h1>
+
+
+<?php 
+global $wpdb;
+
+$scores = $wpdb->get_results( 
+	"
+	SELECT a.meta_value, b.post_title FROM {$wpdb->postmeta} a left join {$wpdb->posts} b on a.post_id=b.id
+    WHERE meta_key='_sg_dictation_score' order by `meta_value` desc
+	"
+);
+echo "<ul>";
+foreach ( $scores as $score ) 
+{
+    echo "<li>".$score->meta_value."&nbsp;&nbsp;|&nbsp;&nbsp;".$score->post_title."</li>";
+}
+echo "</ul>";
+
+
+ ?>
