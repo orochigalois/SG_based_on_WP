60e1e47e2f8220ef50569b637969fd3cbcfe55a9
Xin Yin <xinyin@Xins-iMac.lan>
2019-05-10 12:03:06 +1000

    dashboard: the score is currently shown for everyone #15

--- wp-content/themes/sg/partials/widget_roadmap.php
+++ wp-content/themes/sg/partials/widget_roadmap.php
@@ -2,12 +2,12 @@
 
 <?php 
 global $wpdb;
-
+global $current_user;
 $scores = $wpdb->get_results( 
 	"
 	SELECT a.meta_value, b.post_title FROM {$wpdb->postmeta} a left join {$wpdb->posts} b on a.post_id=b.id
-    WHERE meta_key='_sg_dictation_score' order by `meta_value` desc
-	"
+    WHERE meta_key='_sg_dictation_score' and b.post_author='{$current_user->ID}' order by `meta_value` desc
+    "
 );
 echo "<ul>";
 foreach ( $scores as $score ) 
