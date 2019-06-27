feea6f3e9f3ceef2c94021a075a073ef8d66a113
Xin Yin <xinyin@Xins-iMac.lan>
2019-06-27 15:36:21 +1000

    add ajax error handler

--- wp-content/themes/sg/dist/js/_library.min.js
+++ wp-content/themes/sg/dist/js/_library.min.js
@@ -172,6 +172,17 @@ function initOpenModal() {
 				}
 
 			},
+			error: function(jqXHR, textStatus, errorThrown) {
+                alert('An error occurred... Look at the console (F12 or Ctrl+Shift+I, Console tab) for more information!');
+
+                alert('<p>status code: '+jqXHR.status+'</p><p>errorThrown: ' + errorThrown + '</p><p>jqXHR.responseText:</p><div>'+jqXHR.responseText + '</div>');
+                console.log('jqXHR:');
+                console.log(jqXHR);
+                console.log('textStatus:');
+                console.log(textStatus);
+                console.log('errorThrown:');
+                console.log(errorThrown);
+            },
 		});
 	});
 }
